e18287c9c8535362548894f08e07ceb1077c7bf4
Clint Johnson <Clint.Johnson.07@gmail.com>
2015-10-16 18:21:19 -0400

    Working on boostrap

--- 204/index.rb
+++ 204/index.rb
@@ -45,12 +45,21 @@ def setup_index_view
   @message = get_message(birth_path_num)
 end
 
+get '/message/:birth_path_num' do
+  birth_path_num = params[:birth_path_num].to_i
+  @message = get_message(birth_path_num)
+  erb :index
+end
+
 get '/' do
   erb :form
 end
 
 post '/' do
-    setup_index_view
+  birthdate = params[:birthdate].gsub("-","")
+  birth_path_num = get_birth_path_num(params[:birthdate])
+  redirect "/message/#{birth_path_num}"
+  # setup_index_view
 end
 
 get '/newpage' do
