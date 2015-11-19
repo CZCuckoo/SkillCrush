47caccf9e1e60aab43eeb5378db502bc7b09c316
Clint Johnson <Clint.Johnson.07@gmail.com>
2015-11-19 18:40:50 -0500

    Working on 3 letter anagram.

---
+++ 204/Anagram/starter-code/anagram.rb
@@ -0,0 +1,20 @@
+require_relative 'reverse_letters'
+
+
+
+
+
+
+
+method takes a string as an argument
+  method(argument)
+splits string in to array using .split split(",")
+  split = argument.split(//)
+
+
+
+
+returns array of strings
+
+
+reverse letters takes an array and puts it in reverse
