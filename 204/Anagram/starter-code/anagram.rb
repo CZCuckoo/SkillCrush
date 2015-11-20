be885b0cf574b594ae75abe24a242bb161053192
Clint Johnson <Clint.Johnson.07@gmail.com>
2015-11-19 20:47:17 -0500

    3 letter anagram done, but not as I expected

--- 204/Anagram/starter-code/anagram.rb
+++ 204/Anagram/starter-code/anagram.rb
@@ -1,20 +1,33 @@
-require_relative 'reverse_letters'
+def find_anagram(input)
+  test = input.split(//)
+  test2 = Array.new
+  test2 = test.permutation.map(&:join)
+end
 
 
 
-
-
-
-
-method takes a string as an argument
-  method(argument)
-splits string in to array using .split split(",")
-  split = argument.split(//)
-
-
-
-
-returns array of strings
-
-
-reverse letters takes an array and puts it in reverse
+#
+#
+# method takes a string as an argument
+#   method(argument)
+# splits string in to array using .split split(",")
+#   split = argument.split(//)
+#
+#
+#
+#
+# returns array of strings
+#
+#
+#
+# .select
+# .join
+#
+# test = ["a", "b", "c"]
+# test2[0] = test.join
+# test2[1] = test.reverse.join
+#
+#
+# test.permutation.to_a
+#   returns [["a", "b", "c"] ["b", "a", "c"]]
+#
