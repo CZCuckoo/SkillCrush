967a319bfedc0e9cc38cd9790488f9d39169a9d1
Clint Johnson <Clint.Johnson.07@gmail.com>
2015-11-23 21:08:22 -0500

    3 letter anagrams working

---
+++ 204/Anagram/lesson16-204-mvc-skeleton/app/controllers/index_controller.rb
@@ -0,0 +1,7 @@
+get '/' do
+  erb :index
+end
+
+get '/' do
+  erb :form
+end
--- 204/Anagram/starter-code/anagram.rb
+++ 204/Anagram/starter-code/anagram.rb
@@ -1,7 +1,25 @@
 def find_anagram(input)
   test = input.split(//)
-  test2 = Array.new
-  test2 = test.permutation.map(&:join)
+  test = test.permutation.map(&:join)
+end
+
+
+
+require_relative 'reverse_letters'
+
+def find_anagram(letters)
+  length = letters.length
+  letters = letters.split("")
+
+  anagrams = []
+
+  letters.each_with_index do |letter, index|
+    remaining_letters = letters.select {|l| l != letter}
+    anagrams << letter + remaining_letters.join
+
+    anagrams << letter + reverse_letters(remaining_letters).join
+  end
+  anagrams
 end
 
 
