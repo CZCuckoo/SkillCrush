1ebb3bbbb9bb7b56f1de838ec274cd7a8a5be688
Clint Johnson <Clint.Johnson.07@gmail.com>
2015-12-16 17:38:57 -0500

    Working on anagram. uniq not working

--- 204/Anagram/lesson16-204-mvc-skeleton/app/controllers/index_controller.rb
+++ 204/Anagram/lesson16-204-mvc-skeleton/app/controllers/index_controller.rb
@@ -2,13 +2,44 @@ get '/' do
   erb :index
 end
 
+post '/' do
+  @word = params[:word]
+  if valid_input?(@word)
+    redirect "/anagrams/#{@word}"
+  else
+    @error = "Your entry is not valid"
+    erb :index
+  end
+end
+
 get '/anagrams/:word' do
   @word = params[:word]
   @anagrams = Word.find_anagrams(@word)
   erb :show
 end
 
-post '/' do
-  @word = params[:word]
-  redirect "/anagrams/#{@word}"
+def three_letters?(input)
+  if input.length > 3
+    return false
+  else
+    return true
+  end
+end
+
+def distinct_letters?(input)
+  letter_array = input.chars
+  unique_letters = input.uniq
+  if unique_letters.length < letter_array.length
+    false
+  else
+    true
+  end
+end
+
+def valid_input?(input)
+  if three_letters?(input) && distinct_letters?(input)
+    true
+  else
+    false
+  end
 end
--- 204/Anagram/lesson16-204-mvc-skeleton/app/models/word.rb
+++ 204/Anagram/lesson16-204-mvc-skeleton/app/models/word.rb
@@ -4,31 +4,54 @@ class Word < ActiveRecord::Base
     @anagrams = string.split(//)
     @anagrams = @anagrams.permutation.map(&:join)
   end
-  #
+
+
+  def self.find_anagrams(string)
+    # Convert word to an array of letters
+    letters = string.split(//)
+
+    # Create an array to store our anagrams
+    @combinations = []
+
+    # Loop through each letter in letters
+    letters.each do |letter|
+      # Select the remaining letters
+      remaining = letters.select { |l| l != letter }
+
+      # Create a new word by combining the letter + the remaining letters
+      # Add new word to anagrams array
+      @combinations << letter + remaining.join('')
+
+      # Create a new word by combining the letter + the reverse of the remaining letters
+      # Add new word to anagrams array
+      @combinations << letter + reverse_letters(remaining).join('')
+    end
+
+    @anagrams = []
+    @combinations.each do |combo|
+      if Word.find_by_text(combo).present?
+        @anagrams << combo
+      end
+    end
+
+    # Return anagrams array
+    @anagrams
+  end
+
+
   # def self.find_anagrams(string)
-  #   # Convert word to an array of letters
-  #   letters = string.split(//)
-  #
-  #   # Create an array to store our anagrams
+  #   @combinations = []
+  #   @combinations = string.split(//)
+  #   @combinations = @combinations.permutation.map(&:join)
   #   @anagrams = []
-  #
-  #   # Loop through each letter in letters
-  #   letters.each do |letter|
-  #     # Select the remaining letters
-  #     remaining = letters.select { |l| l != letter }
-  #
-  #     # Create a new word by combining the letter + the remaining letters
-  #     # Add new word to anagrams array
-  #     @anagrams << letter + remaining.join('')
-  #
-  #     # Create a new word by combining the letter + the reverse of the remaining letters
-  #     # Add new word to anagrams array
-  #     @anagrams << letter + reverse_letters(remaining).join('')
+  #   @combinations.each do |combo|
+  #     if Word.find_by_text(combo).present?
+  #       @anagrams << combo
+  #     end
   #   end
-  #
-  #   # Return anagrams array
   #   @anagrams
   # end
+
 
   def self.reverse_letters(letters)
     # create a new array of 2 items
